e600c6c8a158de55c0e58da87770c193dc690e73
madsur <dennissheets@outlook.com>
2021-06-12 20:51:04 -0500

    add exceptions to the buil.gradle file to fix Unit Testing

--- queries.sql
+++ queries.sql
@@ -1,7 +1,23 @@
 ## Part 1: Test it with SQL
 
+        SHOW COLUMNS FROM job
+
+        --
+        --
+        --
+        --
+
 ## Part 2: Test it with SQL
 
+        SELECT *
+        FROM employer
+        WHERE location="St. Louis City"
+
 ## Part 3: Test it with SQL
+
+         -- Had to drop the job_skills table first (foreign key constraint on table job_skills)
+
+        DROP TABLE job_skills;
+        DROP TABLE job
 
 ## Part 4: Test it with SQL
